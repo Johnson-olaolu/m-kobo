7579d6a9ee727cbc3ec92223caa5cc7cc7ac9ee5
Olaolu Johnson <johnsonolaolu@gmail.com>
2022-09-20 04:48:31 +0100

    updated

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,47 +1,69 @@
 import { NextPage } from 'next';
 import React from 'react';
-import PropTypes from 'prop-types';
 import CircularPercentage from '../../assets/icons/CircularPercentage';
 import CardStyles from './card.module.scss';
 import BigEyeIcon from '../../assets/icons/BigEyeIcon';
+import SmallEyeIcon from '../../assets/icons/SmallEyeIcon';
 
 const Card: NextPage<{
   type : 'main' | 'plan'
+  title : string
+  badge : string
+  value : string
+  interest? : string
+  color : string
 }> = (props) => {
-  const { type } = props;
+  const {
+    type, color, title, badge, value, interest,
+  } = props;
   return (
-    <div className={CardStyles.card}>
-      <div className={CardStyles.cardHeader}>
-        <p className={CardStyles.cardTitle}>Available Balance</p>
+    <div className={CardStyles.card} style={{ backgroundColor: color }}>
+      <div
+        className={CardStyles.cardHeader}
+        style={{
+          color: type === 'main' ? 'rgba(255, 255, 255, 0.5)' : 'rgba(38, 50, 56, 0.5)',
+        }}
+      >
+        <p className={CardStyles.cardTitle}>{title}</p>
         {type === 'main' ? (
-          <p className={CardStyles.cardBadge}>Tier1</p>
+          <p className={CardStyles.cardBadge}>{badge}</p>
         ) : (
-          <p className="">
-            75% Achieved
+          <p className={CardStyles.cardBadge2}>
+            {badge}
             <CircularPercentage />
           </p>
         )}
       </div>
-      <div className={CardStyles.cardBody}>
+      <div
+        className={CardStyles.cardBody}
+        style={{
+          color: type === 'main' ? '#FFFFFF' : '#263238',
+        }}
+      >
         <span className={CardStyles.cardUnit}>₦</span>
-        <p className={CardStyles.cardValue}>6,390.68</p>
+        <p
+          className={CardStyles.cardValue}
+        >
+          {value}
+
+        </p>
         <div className="">
-          <BigEyeIcon />
+          {type === 'main' ? (
+            <BigEyeIcon />
+          ) : (
+            <SmallEyeIcon />
+          )}
         </div>
       </div>
+      {type === 'plan' && (
       <div className={CardStyles.cardFooter}>
-        <p className={CardStyles.cardInterest} />
+        <p className={CardStyles.cardInterest}>
+          {interest}
+        </p>
       </div>
+      )}
     </div>
   );
-};
-
-Card.propTypes = {
-  type: PropTypes.string as any,
-};
-
-Card.defaultProps = {
-  type: 'main',
 };
 
 export default Card;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,56 @@ import React from 'react';
 import Card from '../components/card';
 import HomePageStyles from './HomePage.module.scss';
 
-const Home: NextPage = () => (
-  <div className={HomePageStyles.container}>
-    <h1 className={HomePageStyles.header}>Hello Bennedict,</h1>
-    <div className={HomePageStyles.cardHeader}>
-      <Card type="main" />
-      <Card type="plan" />
-      <div className={HomePageStyles.profileCard}>
-        Profile1
+const Home: NextPage = () => {
+  const cardDetails: {
+    type: 'main' | 'plan';
+    title: string;
+    badge: string;
+    value: string;
+    interest?: string;
+    color: string;
+  }[] = [
+    {
+      title: 'Avalaible Balance',
+      type: 'main',
+      badge: 'Tier 1',
+      value: '6,390.68',
+      color: '#0047CC',
+    },
+    {
+      title: 'Car Purchase Plan',
+      type: 'plan',
+      badge: '75% Achieved',
+      value: '6,390.68',
+      color: '#D4D4FF',
+      interest: 'Interest as at today:  : ₦20,000',
+    },
+    {
+      title: 'Car Purchase Plan',
+      type: 'plan',
+      badge: '75% Achieved',
+      value: '6,390.68',
+      color: '#FFD4DF',
+      interest: 'Interest as at today:  : ₦20,000',
+    },
+  ];
+  return (
+    <div className={HomePageStyles.container}>
+      <h1 className={HomePageStyles.header}>Hello Bennedict,</h1>
+      <div className={HomePageStyles.cardHeader}>
+        {cardDetails.map((card) => (
+          <Card
+            type={card.type}
+            badge={card.badge}
+            color={card.color}
+            title={card.title}
+            value={card.value}
+            interest={card.interest}
+          />
+        ))}
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
